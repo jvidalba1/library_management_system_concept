df5ea1d5136007831a09d497fdfdb7a5b3b15f7e
Mateo Vidal <jvidalba@gmail.com>
2023-12-19 15:54:37 -0500

    refactoring dashboard with query objects

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # ToDo Query Objects
-    @books = Book.all
-    @borrowed_books = Book.includes(:borrowings).where(borrowings: { status: 1 })
-    @books_due_today = @borrowed_books.where(borrowings: { due_at: Time.current.beginning_of_day..Time.current.end_of_day})
-    @users = User.includes(:borrowings).where(borrowings: { status: 3 })
-
-    @member_borrowings = Borrowing.includes(:books).where(borrowings: { user_id: current_user.id, status: [1,2,3]} )
+    @librarian_dashboard = ::CreateLibrarianDashboard.new(Book.all).call
+    @member_dashboard = CreateMemberDashboard.new(current_user).call
   end
 end
